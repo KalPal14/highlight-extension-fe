ae25846e8e12c99f5653730e426a12f17ccf8115
KalPal <reznichenkomykola1@gmail.com>
2024-06-19 12:00:43 +0300

    fix/bug with searching for an element by text

--- src/content-scripts/highlights/helpers/to-receive-DOM-data/find-elements-by-text.helper.ts
+++ src/content-scripts/highlights/helpers/to-receive-DOM-data/find-elements-by-text.helper.ts
@@ -4,12 +4,15 @@ export default function findElementsByText(
 	text: string,
 	startElement: Element = document.body
 ): Element[] {
+	const onlyChartersText = text.replace(/\s/g, '');
 	const result: Element[] = [];
 
 	find(startElement);
 
 	function find(element: Element): void {
-		if (element.textContent === text && element.tagName !== 'WEB-HIGHLIGHT') {
+		if (!element.textContent) return;
+		const onlyChartersTextContent = element.textContent.replace(/\s/g, '');
+		if (onlyChartersTextContent === onlyChartersText && element.tagName !== 'WEB-HIGHLIGHT') {
 			result.push(element);
 			setInitialTextToHighlightPerent(element);
 		}
